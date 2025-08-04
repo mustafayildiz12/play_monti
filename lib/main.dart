dc9d8f520338313e5dc4470cc104f5eef3006844
mustafayildiz12 <musttafayildiz12@gmail.com>
2025-08-04 15:21:19 +0300

    Changelog: => firebase connection done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:play_monti/constants/app_translation.dart';
+import 'package:play_monti/firebase_options.dart';
 import 'package:play_monti/screens/tab_screens.dart';
 import 'package:provider/provider.dart';
 import 'contexts/user_context.dart';
 import 'screens/onboarding_flow.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
